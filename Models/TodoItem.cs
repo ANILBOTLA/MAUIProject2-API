4309f86b2a00536be860228d7371506d0278e856
Anil Botla <botlaanil007@gmail.com>
2022-11-04 19:55:24 -0500

    Add project files.

---
+++ Models/TodoItem.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoAPI.Models
+{
+    public class TodoItem
+    {
+        [Required]
+        public string? ID { get; set; }
+
+        [Required]
+        public string? Expression { get; set; }
+
+        [Required]
+        public string? Option1 { get; set; }
+
+        [Required]
+        public string? Option2 { get; set; }
+
+        [Required]
+        public string? Option3 { get; set; }
+
+  
+    }
+}
